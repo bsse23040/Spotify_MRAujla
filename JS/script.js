ad2b9e29096fe3226a64cdb51fbedb0f9cae574f
bsse23040 <143793936+bsse23040@users.noreply.github.com>
2024-07-09 16:00:55 +0500

    ok

--- JS/script.js
+++ JS/script.js
@@ -131,9 +131,9 @@ async function displayAlbums() {
 
 // main function
 async function main() {
-    songs = await getSongs('musics/A1_Karan_Aujla');
-    playMusic(songs[0], true);
-    console.log(`track= ${songs[0]}`);
+    // songs = await getSongs('musics/A1_Karan_Aujla');
+    // playMusic(songs[0], true);
+    // console.log(`track= ${songs[0]}`);
     
     // Display the list of all the songs
     displayAlbums();
