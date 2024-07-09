6284ae0ea37b584d11019a1ca18265c0cc05e99c
bsse23040 <143793936+bsse23040@users.noreply.github.com>
2024-07-09 17:38:43 +0500

    ok

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 // Global variables
-const currentSong = new Audio();
+let currentSong = new Audio();
 let songs = [];
-let currFolder = "";
+let currFolder;
 
 // Time formatting function
 function formatTime(seconds) {
@@ -16,35 +16,30 @@ function formatTime(seconds) {
     return minutes + ':' + secs;
 }
 
-// Function to get songs from a folder
+// Function to get songs in array from folder
 function getSongs(folder) {
     currFolder = folder;
     let div = document.createElement("div");
-    div.innerHTML = document.body.innerHTML;
+    div.innerHTML = `
+        <a href="${folder}/song1.mp3"></a>
+        <a href="${folder}/song2.mp3"></a>
+        <a href="${folder}/nextsong.mp3"></a>
+    `;
     let as = div.getElementsByTagName("a");
-    songs = [];
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/assets/${currFolder}/`)[1]);
-        }
-    }
-
-    // Display all songs in the playlist
+    songs = Array.from(as).map(a => a.href.split(`${folder}/`)[1]);
+    
+    // Update playlist UI
     let songUL = document.querySelector(".songList ul");
     songUL.innerHTML = "";
     songs.forEach(song => {
         let index1 = song.indexOf("Artist");
         let index2 = song.indexOf(".mp3");
-
         let mysongname = '';
         let myartistname = '';
-
         if (index1 !== -1 && index2 !== -1 && index1 < index2) {
             mysongname = song.substring(0, index1).replaceAll("%20", " ");
             myartistname = song.substring((index1 + 6), index2).replaceAll("%20", " ");
         }
-
         songUL.innerHTML += `<li>
                                 <img class="invert" src="assets/svgs/music.svg" alt="music icon">
                                 <div class="info">
@@ -58,8 +53,8 @@ function getSongs(folder) {
                              </li>`;
     });
 
-    // Attach event listeners to each song
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+    // Attach event listeners to songs
+    Array.from(songUL.getElementsByTagName("li")).forEach(e => {
         e.addEventListener("click", () => {
             let songName = e.querySelector(".info div:first-child").textContent.trim();
             let artistName = e.querySelector(".info div:last-child").textContent.trim();
@@ -70,8 +65,36 @@ function getSongs(folder) {
     return songs;
 }
 
-// Function to play music
-function playMusic(track, pause = false) {
+// Function to display albums from local data
+function displayAlbums() {
+    let cardContainer = document.querySelector(".cardContainer");
+    let albums = [
+        { folder: "A1_Karan_Aujla", title: "Aujla Anthems", description: "Feel the energy and passion of Karan Aujla's powerful Punjabi tracks." },
+        { folder: "Turkish_songs", title: "Turkish Songs", description: "Discover the melodious Turkish tunes that will captivate your soul." }
+        // Add more albums as needed
+    ];
+
+    albums.forEach(album => {
+        cardContainer.innerHTML += `<div data-folder="${album.folder}" class="card">
+            <div class="play">
+                <img src="https://img.icons8.com/sf-black-filled/64/play.png" alt="play" />
+            </div>
+            <img src="assets/musics/${album.folder}/cover.jpeg" alt="cover pic">
+            <h2>${album.title}</h2>
+            <p>${album.description}</p>
+        </div>`;
+    });
+
+    // Attach event listeners to each album card
+    Array.from(cardContainer.getElementsByClassName("card")).forEach(e => {
+        e.addEventListener("click", async () => {
+            await getSongs(`musics/${e.dataset.folder}`);
+        });
+    });
+}
+
+// Music playing function
+const playMusic = (track, pause = false) => {
     currentSong.src = `assets/${currFolder}/${track}`;
     if (!pause) {
         currentSong.play();
@@ -81,42 +104,11 @@ function playMusic(track, pause = false) {
     document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
 }
 
-// Function to display albums
-function displayAlbums() {
-    let anchors = Array.from(document.getElementsByTagName("a"));
-    let cardContainer = document.querySelector(".cardContainer");
-    anchors.forEach(e => {
-        if (e.href.includes("/musics") && !e.href.includes(".htaccess")) {
-            let folder = e.href.split('/').slice(-2)[0];
-            fetch(`assets/musics/${folder}/info.json`)
-                .then(response => response.json())
-                .then(data => {
-                    cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
-                        <div class="play">
-                            <img src="https://img.icons8.com/sf-black-filled/64/play.png" alt="play" />
-                        </div>
-                        <img src="assets/musics/${folder}/cover.jpeg" alt="cover pic">
-                        <h2>${data.title}</h2>
-                        <p>${data.description}</p>
-                    </div>`;
-                })
-                .catch(error => console.error('Error fetching album info:', error));
-        }
-    });
-
-    // Load event listeners when cards are clicked
-    Array.from(document.getElementsByClassName("card")).forEach(e => {
-        e.addEventListener("click", item => {
-            getSongs(`musics/${item.currentTarget.dataset.folder}`);
-        });
-    });
-}
-
 // Main function
-function main() {
+async function main() {
     displayAlbums();
 
-    // Play/pause functionality
+    // Song play pause on click
     const playWrapper = document.getElementById("play-wrapper");
     playWrapper.addEventListener("click", () => {
         if (currentSong.paused) {
@@ -128,8 +120,8 @@ function main() {
         }
     });
 
-    // Spacebar play/pause
-    document.addEventListener('keydown', event => {
+    // Song play pause on space bar
+    document.addEventListener('keydown', function (event) {
         if (event.code === 'Space') {
             if (currentSong.paused) {
                 currentSong.play();
@@ -141,20 +133,20 @@ function main() {
         }
     });
 
-    // Update time and seekbar
+    // Listen for time update
     currentSong.addEventListener("timeupdate", () => {
         document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)} / ${formatTime(currentSong.duration)}`;
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
     });
 
-    // Seekbar functionality
+    // Add an event listener to seekbar
     document.querySelector(".seekbar").addEventListener("click", e => {
         let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
         document.querySelector(".circle").style.left = percent + "%";
         currentSong.currentTime = (currentSong.duration) * percent / 100;
     });
 
-    // Previous and next buttons
+    // Add event listener to previous and next button
     document.getElementById("previous").addEventListener("click", () => {
         let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
         if (index > 0) {
@@ -169,22 +161,32 @@ function main() {
         }
     });
 
-    // Volume control and visibility
+    // Adjusting the volume and updating the volume image on input change
     document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
         currentSong.volume = parseInt(e.target.value) / 100;
         const volumeImg = document.querySelector(".volume > img");
-        volumeImg.src = currentSong.volume === 0 ? "assets/svgs/mute.svg" : "assets/svgs/volume.svg";
+        if (currentSong.volume === 0) {
+            volumeImg.src = "assets/svgs/mute.svg";
+        } else {
+            volumeImg.src = "assets/svgs/volume.svg";
+        }
     });
 
+    // Volume range colorer
     document.querySelector('.my-range').addEventListener('input', function () {
         this.style.setProperty('--value', this.value + '%');
     });
 
+    // Controlling the visibility of the volume range
     document.querySelector('.volume').addEventListener('click', function () {
         const rangeElement = document.querySelector('.range');
-        rangeElement.style.visibility = rangeElement.style.visibility === 'hidden' ? 'visible' : 'hidden';
+        if (rangeElement.style.visibility === 'hidden') {
+            rangeElement.style.visibility = 'visible';
+        } else {
+            rangeElement.style.visibility = 'hidden';
+        }
     });
 }
 
-// Call main function when the document is fully loaded
+// Run main function when the document is ready
 document.addEventListener("DOMContentLoaded", main);
