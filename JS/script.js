3ea2dc2ce8281b5c3e522b8d5a9eca09853eb44c
bsse23040 <143793936+bsse23040@users.noreply.github.com>
2024-08-11 22:53:43 +0500

    ok

--- JS/script.js
+++ JS/script.js
@@ -28,18 +28,18 @@ function getSongs(folder) {
     let div = document.createElement("div");
     div.innerHTML = `
     <a href="${folder}/On Top Artist Karan Aujla I llky.mp3" data-folder="A1_Karan_Aujla"></a>
-    <a href="${folder}/Bachke Bachke Artist Karan Aujla.mp3" data-folder="A1_Karan_Aujla"></a>
-    <a href="${folder}/Making Memories Artist Karan Aujla.mp3" data-folder="A1_Karan_Aujla"></a>
+    <a href="${folder}/Maxico Artist Karan Aujla.mp3" data-folder="A1_Karan_Aujla"></a>
+    <a href="${folder}/Kya Baat Aa Artist Karan Aujla.mp3" data-folder="A1_Karan_Aujla"></a>
     <a href="${folder}/Softly Artist Karan Aujla I llky.mp3" data-folder="A1_Karan_Aujla"></a>
     <a href="${folder}/Winning Speech Artist Karan Aujla I Mxrci (8D).mp3" data-folder="A1_Karan_Aujla"></a>
     <a href="${folder}/Alone Artist Alan Walker.mp3" data-folder="Alan_Walker"></a>
+    <a href="${folder}/Alone 2 Artist Alan Walker.mp3" data-folder="Alan_Walker"></a>
     <a href="${folder}/PUBG X On My Way Artist Alan Walker.mp3" data-folder="Alan_Walker"></a>
     <a href="${folder}/Faded Artist Alan Walker.mp3" data-folder="Alan_Walker"></a>
     <a href="${folder}/Aujla Mashup Artist Karan Aujla.mp3" data-folder="Gangsta_Mashup"></a>
     <a href="${folder}/All Mashup 1 Artist Sidhu x Shubh X AP Dhillon.mp3" data-folder="Gangsta_Mashup"></a>
     <a href="${folder}/All Mashup 2 Artist Diljit Dosanjh x Varinder Barar.mp3" data-folder="Gangsta_Mashup"></a>
     <a href="${folder}/Beqarar Karke Hume Artist Hemant Kumar.mp3" data-folder="Pure_Olds"></a>
-    <a href="${folder}/Dekha Aik Khawab Artist Kishore Kumar for Amitabh Bachan.mp3" data-folder="Pure_Olds"></a>
     <a href="${folder}/Ramaiya Vastavaiya Artist Mohammed Rafi.mp3" data-folder="Pure_Olds"></a>
     <a href="${folder}/Yeh Ratein Yeh Mausam Artist  Kishore Kumar.mp3" data-folder="Pure_Olds"></a>
     <a href="${folder}/Dedubluman You don't know Artist Sen Bilmezsin.mp3" data-folder="Turkish_Songs"></a>
